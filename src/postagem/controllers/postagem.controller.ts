e7088f53cbc286406f05a0d9589ce06ae01ae0cb
WallaceArauj <wallace.hostile94@gmail.com>
2022-10-31 16:33:28 -0300

    fix: added crud features postagem.controller

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus, ParseIntPipe, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 import { Postagem } from "../../postagem/entities/postagem.entity";
@@ -9,8 +9,8 @@ import { PostagemService } from "../../postagem/services/postagem.service";
 @Controller('/postagem')
 @ApiBearerAuth()
 export class PostagemContoller {
-
-    constructor(private readonly postagemService: PostagemService) { }
+    
+     constructor(private readonly postagemService: PostagemService) { }
 
     @UseGuards(JwtAuthGuard)
     @Get('/postagem')
@@ -18,5 +18,34 @@ export class PostagemContoller {
     findAll(): Promise<Postagem[]> {
         return this.postagemService.findAll()
     }
-       
+    
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise <Postagem> {
+        return this.postagemService.findById(id);
+    }
+
+    findbyDescricao(@Param('descricao') descricao: string): Promise<Postagem[]> {
+        return this.postagemService.findByDescricao(descricao);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.create(postagem);       
+    }
+
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.update(postagem);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.postagemService.delete(id)
+    }
+
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Postagem } from "../../postagem/entities/postagem.entity";
-import { Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 
 @Injectable()
 export class PostagemService {
@@ -9,16 +9,23 @@ export class PostagemService {
     constructor(
         @InjectRepository(Postagem)
         private postagemRepository: Repository<Postagem> //assíncrona
-    ) { }
+    ) {}
 
     async findAll(): Promise<Postagem[]> {
-        return await this.postagemRepository.find()
+        return await this.postagemRepository.find({
+            relations: {
+                tema: true
+            }
+        });
     }
 
     async findById (id: number): Promise <Postagem> {
         let postagem = await this.postagemRepository.findOne({
             where: {
                 id
+            },
+            relations: {
+                tema: true
             }
         });
 
@@ -26,6 +33,37 @@ export class PostagemService {
         throw new HttpException('Tema não encontrado!' ,HttpStatus.NOT_FOUND);
 
         return postagem; 
+    }
+
+    async findByDescricao (descricao: string): Promise <Postagem[]>{
+        return await this.postagemRepository.find({})
+
+    }
+
+    
+    async create (postagem: Postagem): Promise<Postagem>{
+    return await this.postagemRepository.save(postagem);
+
+    }
+
+    async update (postagem: Postagem): Promise<Postagem>{
+
+        let buscaPostagem = await this.findById(postagem.id);
+
+        if(!buscaPostagem || !postagem.id)
+        throw new HttpException ('Tema não encontrado!',HttpStatus.NOT_FOUND);
+
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async delete (id: number): Promise<DeleteResult> {
+        let buscaPostagem = await this.findById(id);
+
+        if (!buscaPostagem)
+        throw new HttpException ('Postagem não Encontrado!', HttpStatus.NOT_FOUND);
+
+        return await this.postagemRepository.delete(id);
+
     }
 
 }
