39c66e5ad607ad75562a3f6ad8c5333d530dff36
WallaceArauj <wallace.hostile94@gmail.com>
2022-10-31 10:40:59 -0300

    feat: preparing to deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,28 @@ import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_blogpessoal',
-      entities: [__dirname + '/**/*.entity{.js,.ts}'],
-      synchronize: true
-    }),
+    //  TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'db_blogpessoal',
+    //   entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    //   synchronize: true
+    //   }), 
+    
+        TypeOrmModule.forRoot({
+        type: 'postgres',
+        url: 'process.env.DATABASE_URL',
+        logging: false,
+        dropSchema: false,
+        ssl: {
+          rejectUnauthorized: false
+        },
+        synchronize: true,
+        autoLoadEntities: true
+      }),
     PostagemModule,
     TemaModule,
     AuthModule,
